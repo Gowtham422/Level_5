499bed3d78ea3e54fa526d5055a7effb909c7ef2
Gowtham422 <63245218+Gowtham422@users.noreply.github.com>
2020-04-07 11:01:02 +0530

    Add files via upload

---
+++ Garbage_Collector.java
@@ -0,0 +1,20 @@
+package pkg_1;
+
+public class Garbage_Collector {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str1 = "gkgkjhkjhkjj";
+		
+		for (int i = 1; i <= str1.length()-1; i++) {
+			String temp = "ch" +i+ str1.charAt(i);
+			
+			System.out.println(str1);
+		}
+		
+		
+
+	}
+
+}
